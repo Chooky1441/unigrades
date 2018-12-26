f69ee43aa42bee4bbe8be6762ae2c13014a8315a
Chooky1441 <anthonynavarrette2017@gmail.com>
2018-12-26 14:58:59 -0800

    Create Schedule screen complete

--- source/create_schedule.py
+++ source/create_schedule.py
@@ -1,18 +1,58 @@
 from kivy.uix.screenmanager import Screen, SlideTransition
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import StringProperty
 import utils
 
 class CreateSchedule(Screen):
 
+    _name, _gpa, _units = StringProperty(), StringProperty(), StringProperty()
+
     def __init__(self, **kwargs):
-        self._name = StringProperty('')
-        self._gpa = NumericProperty(0)
-        self._units = NumericProperty(0)
         super(Screen, self).__init__(**kwargs)
 
-    def back(self) -> None:
-        self.parent.transition = SlideTransition(direction = 'right', duration = utils.DEFAULT_TRANSITION_LENGTH)
-        self.parent.current = 'home_screen'
+    def _clear_text_fields(self):
+        self._name = ''
+        self._gpa = ''
+        self._units = ''
 
-    def create(self):
-        pass
+    def back(self) -> None:
+
+        def slide_back():
+            self.parent.transition = SlideTransition(direction = 'right', duration = utils.DEFAULT_TRANSITION_LENGTH)
+            self.parent.current = 'home_screen'
+
+        if (self._name == '' and self._gpa == '' and self._units == ''):
+            slide_back()
+        else:
+
+            def yes_func():
+                self._clear_text_fields()
+                slide_back()
+
+            utils.yesno_popup('Are you sure you want to go back?\nThis schedule will not be saved.', yes_func)
+
+    def create(self) -> None:
+
+        if self._name.replace(' ', '') != '':         # if the name is not empty space or just spaces
+            gpa_f = 0
+            try:                                        # make sure the GPA is a number
+                gpa_f = float(self._gpa)
+            except ValueError:
+                utils.default_popup('G.P.A. must be a number.')
+            else:
+                if gpa_f < 0:                           # make sure the GPA is >= 0
+                    utils.default_popup('G.P.A. must be greater than zero.')
+                else:
+                    try:
+                        units_f = float(self._units)    # make sure the units is a number
+                    except ValueError:
+                        utils.default_popup('Previous units taken must be a number.')
+                    else:
+                        if (units_f < 0):               # make sure the units is >= 0
+                            utils.default_popup('Previous units taken must be greater than zero.')
+                        elif (int(units_f) != units_f): # make sure the units is a whole number
+                            utils.default_popup('Previous units taken must be a whole number')
+                        else:
+                            self._clear_text_fields()
+                            pass # create a new schedule
+        else:
+            utils.default_popup('Schedule name cannot be left blank.')
--- source/utils.py
+++ source/utils.py
@@ -1,3 +1,12 @@
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+DG = 0.13
+G = 0.18
+LG = 0.26
+
 DEFAULT_TRANSITION_LENGTH = 0.15
 
 TO_GPA = {'A': 4.0, 'A-': 3.7, 'B+': 3.3, 'B': 3.0, 'B-': 2.7, 'C+': 2.3, 'C': 2.0, 'C-': 1.7, 'D+': 1.3, 'D': 1.0, 'D-': 0.7, 'F': 0.0}
@@ -5,6 +14,39 @@ TO_GPA = {'A': 4.0, 'A-': 3.7, 'B+': 3.3, 'B': 3.0, 'B-': 2.7, 'C+': 2.3, 'C': 2
 def to_gpa(grade: str) -> float:
     """returns the gpa the student has based on the letter grade (4.0 scale)"""
     return TO_GPA[grade]
+
+def default_popup(text: str) -> None:
+    err_box = BoxLayout(orientation = 'vertical', padding = (10))
+    err_box.add_widget(Label(text = text))
+    close_button = Button(text = "Close")
+    err_box.add_widget(close_button)
+    err = Popup(title = 'Warning', content = err_box, size_hint = (0.6, 0.4))
+    close_button.bind(on_release = err.dismiss)
+    err.open()
+
+def yesno_popup(text: str, yes_func: 'function') -> None:
+    box = BoxLayout(orientation = 'vertical', padding = (10))
+    box.add_widget(Label(text = text))
+
+    button_box = BoxLayout(orientation = 'horizontal')
+    box.add_widget(button_box)
+
+    yes_button = Button(text = 'Yes')
+    no_button = Button(text = 'No')
+
+    button_box.add_widget(yes_button)
+    button_box.add_widget(no_button)
+
+    popup = Popup(title = 'Warning', content = box, size_hint = (0.6, 0.4))
+
+    def new_yes_func(*args):
+        yes_func()
+        popup.dismiss()
+
+    yes_button.bind(on_release = new_yes_func)
+    no_button.bind(on_release = popup.dismiss)
+
+    popup.open()
 
 # custom exceptions
 class InvalidCutPointException(Exception):
